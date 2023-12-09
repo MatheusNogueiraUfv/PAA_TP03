6a359fb126ca825337e90071bd06a0fb1b8d8b1b
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2023-12-08 23:43:58 -0300

    :recycle: limpeza

--- CasamentoExato/menu.c
+++ CasamentoExato/menu.c
@@ -21,6 +21,8 @@ void menu()
                 printf("\n\t %sArquivo a ser lido:%s %s", ORANGE, RESET, arqEntrada);
 
                 fscanf(arq, "%s", padrao);
+
+                textoMinusculo(padrao);
                 printf("\n\t %sPadrao a ser procurado:%s %s\n",ORANGE, RESET, padrao);
 
                 fscanf(arq, "%d", &opcaoAlgoritmo);
@@ -62,6 +64,8 @@ void menu()
 
                 printf("\n\t%sInsira o padrao que será pesquisado:%s\n\t", GREEN, RESET);
                 scanf("%s", padrao);
+
+                textoMinusculo(padrao);
 
                 menu2(arqEntrada, padrao);
 
@@ -118,8 +122,6 @@ void menu2(const char *arqEntrada, char *padrao)
     }
 
 }
-
-
 
 void menu1()
 {
